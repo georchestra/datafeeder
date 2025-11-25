d0af469f449afad28271d9f326423f6d243bcc6e
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-11-25 16:57:53 +0100

    feat(frontend): configure cypress

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      installLogsPrinter(on);
+    },
+  },
+});
+  
