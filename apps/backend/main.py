9409b4cf63069154ff5d31740dcd7a22a9d6da5a
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2025-11-25 17:19:12 +0100

    feat: call data_manipulation lib inside backend

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from data_manipulation import hello
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": hello()}
