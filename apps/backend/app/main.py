bc71a3fa68d344f3d5ab0c5e27db6729334c5848
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2025-11-26 15:05:24 +0100

    feat(backend): add version endpoint

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,5 +1,9 @@
+import importlib.metadata
+
 from data_manipulation import hello
 from fastapi import FastAPI
+
+BACKEND_VERSION = importlib.metadata.version("datakern-backend")
 
 app = FastAPI()
 
@@ -7,3 +11,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": hello()}
+
+
+@app.get("/version")
+def read_version():
+    return {"version": BACKEND_VERSION}
