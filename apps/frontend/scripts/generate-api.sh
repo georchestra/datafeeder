68e5590d1dd41db4d60c5427bc990905407fb829
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-11-26 14:07:03 +0100

    fix(frontend): move frontend to apps/

--- apps/frontend/scripts/generate-api.sh
+++ apps/frontend/scripts/generate-api.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 set -euxo pipefail
 
+APPS_DIR="apps"
 BACKEND_DIR="backend"
 FRONTEND_DIR="frontend"
 OPENAPI_FILE="openapi.json"
 
+cd "$APPS_DIR"
 cd "$BACKEND_DIR"
 mv "$OPENAPI_FILE" "$FRONTEND_DIR/"
 python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > "../$OPENAPI_FILE"
