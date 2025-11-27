e18ba499dc0635672e11706e1338d8bb3058c38a
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-11-27 15:16:02 +0100

    feat(frontend): update e2e to run test on home page

---
+++ apps/frontend/cypress/e2e/home.cy.ts
@@ -0,0 +1,40 @@
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4201');
+  });
+
+  it('should display the home page title', () => {
+    cy.get('h1').should('contain', 'Home page');
+  });
+
+  it('should display the lorem ipsum text', () => {
+    cy.get('p').should('contain', 'Lorem ipsum dolor sit amet');
+  });
+
+  it('should have a primary button', () => {
+    cy.get('button[mat-raised-button]')
+      .should('be.visible')
+      .and('contain', 'Click me');
+  });
+
+  it('should have a checkbox', () => {
+    cy.get('mat-checkbox').should('be.visible').and('contain', 'Check this');
+  });
+
+  it('should be able to click the button', () => {
+    cy.get('button[mat-raised-button]').click();
+  });
+
+  it('should be able to toggle the checkbox', () => {
+    cy.get('mat-checkbox').click();
+    cy.get('mat-checkbox input[type="checkbox"]').should('be.checked');
+    
+    cy.get('mat-checkbox').click();
+    cy.get('mat-checkbox input[type="checkbox"]').should('not.be.checked');
+  });
+
+  it('should have proper spacing and layout', () => {
+    cy.get('.space-y-4').should('exist');
+    cy.get('.flex.items-center.gap-4').should('exist');
+  });
+});
