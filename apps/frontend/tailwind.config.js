fcdece57d56e61a25a9f887270729ca45d8cd9ca
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2025-12-08 14:52:53 +0100

    feat: setup gn-ui, tailwind, font

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+const baseConfig = require('./node_modules/geonetwork-ui/tailwind.base.config')
+const { join } = require('path')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [baseConfig],
+  content: [
+    './node_modules/geonetwork-ui/**/*.mjs',
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#F7F5F0',
+      },
+    },
+  },
+  plugins: [],
+}
