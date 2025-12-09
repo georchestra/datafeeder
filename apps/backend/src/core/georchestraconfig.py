f093f9349980a1f80cb49e470e97b0dbdc3da74b
Jean-Michel Crepel <45998535+jeanmi151@users.noreply.github.com>
2025-12-09 10:20:50 +0100

    change config reading (#22)

---
+++ apps/backend/src/core/georchestraconfig.py
@@ -0,0 +1,79 @@
+from configparser import ConfigParser
+from itertools import chain
+from os import getenv, getcwd
+import json
+import re
+import yaml
+
+class GeorchestraConfig:
+    def __init__(self):
+        self.sections = dict()
+        self.datadirpath = getenv("georchestradatadir", "/etc/georchestra")
+
+        self.read_default()
+        self.read_gateway_routes()
+
+    def read_default(self):
+        parser = ConfigParser()
+        with open(f"{self.datadirpath}/default.properties") as lines:
+            lines = chain(("[section]",), lines)  # This line does the trick.
+            parser.read_file(lines)
+
+        self.sections["default"] = parser["section"]
+        self.sections["default"]["datadirpath"] = self.datadirpath
+
+    def read_gateway_routes(self):
+        with (open(f"{self.datadirpath}/gateway/routes.yaml") as lines):
+            self.sections["gateway_routes"] = dict()
+            lines2 = yaml.safe_load(lines)
+            # only get the targets lines https://github.com/georchestra/datadir/blob/docker-master/gateway/routes.yaml#L76
+            for service_target in lines2["georchestra.gateway.services"]:
+                self.sections["gateway_routes"][service_target] = lines2["georchestra.gateway.services"][service_target]
+
+    def tostr(self):
+        str = ""
+        for key in self.sections:
+            str += key + ":\r\n<br>"
+            for key2 in self.sections[key]:
+                str += " \t&emsp;" + key2 + " : "
+                if self.sections[key][key2] == self.get(key2, section=key):
+                    str += " \t&emsp;" + self.sections[key][key2] + "\r\n<br> "
+                else:
+                    str += (
+                        " \t&emsp;"
+                        + self.sections[key][key2]
+                        + " = "
+                        + self.get(key2, section=key)
+                        + "\r\n<br> "
+                    )
+        return str
+
+    def get(self, key, section="default"):
+        if section not in self.sections:
+            return None
+        value = self.sections[section].get(key, None)
+        if value:
+            # this is to catch ${ENV_VAR}
+            search_env = re.match("^\\${(.*)}$", value)
+            # this is for url using env var http://${ENV_VAR}/geonetwork/..etc?params
+            search_env2 = re.match("(.*)\\${(.*)}(.*)", value)
+            search_env3 = re.match("(.*)\\${(.*):.*}(.*)", value)
+
+            if search_env:
+                if getenv(search_env.group(1)):
+                    value = getenv(search_env.group(1))
+            elif search_env3:
+                if getenv(search_env3.group(2)):
+                    value = (
+                        search_env3.group(1)
+                        + getenv(search_env3.group(2))
+                        + search_env3.group(3)
+                    )
+            elif search_env2:
+                if getenv(search_env2.group(2)):
+                    value = (
+                        search_env2.group(1)
+                        + getenv(search_env2.group(2))
+                        + search_env2.group(3)
+                    )
+        return value
--- apps/backend/src/main.py
+++ apps/backend/src/main.py
@@ -1,10 +1,22 @@
 import importlib.metadata
+import os
 
 from data_manipulation import hello
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 from src.api.main import api_router
+from geonetwork import GnApi
+from src.core.georchestraconfig import GeorchestraConfig
+
+DEBUG=False
+if os.getenv("DEBUG"):
+    value=os.getenv("DEBUG")
+    if "false" == value.lower():
+        DEBUG=False
+    elif "true" == value.lower():
+        DEBUG=True
 
 BACKEND_VERSION = importlib.metadata.version("datakern-backend")
 
@@ -22,6 +34,8 @@ app.add_middleware(
 # Include API router
 app.include_router(api_router)
 
+# Load georchestra properties and merge with settings
+geor_config = GeorchestraConfig()
 
 @app.get("/", tags=["Health"])
 def read_root():
@@ -31,3 +45,14 @@ def read_root():
 @app.get("/version", tags=["Health"])
 def read_version():
     return {"version": BACKEND_VERSION}
+
+@app.get("/geonetwork", tags=["Health"])
+def read_geonetwork():
+    gnapi = GnApi(api_url="http://gateway:8080/geonetwork/srv/api", credentials=None, verifytls=False)
+    return {"Hello": gnapi._get_version().json()}
+
+if DEBUG:
+    @app.get("/config", tags=["Health"], response_class=HTMLResponse)
+    def read_config():
+        print(geor_config.get("domainname","default"))
+        return geor_config.tostr()
