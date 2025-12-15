741acea1db9003c6db79c666cdde1f8531a123b5
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2025-12-15 16:55:03 +0100

    chore(backend): rename import route to ingestion

--- apps/backend/src/api/main.py
+++ apps/backend/src/api/main.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.api.routes import airflow, callbacks, import_router, utils
+from src.api.routes import airflow, callbacks, ingestion, utils
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
 api_router.include_router(airflow.router)
-api_router.include_router(import_router.router)
+api_router.include_router(ingestion.router)
 api_router.include_router(callbacks.router)
