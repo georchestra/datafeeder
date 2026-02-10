a76c9b21720e3637b7d05b3bdcbc133abdd32a56
Florian Necas <florian.necas@camptocamp.com>
2026-02-10 10:18:32 +0100

    fix: query namespace and put datastore with right namespace

--- libs/data_manipulation/src/data_manipulation/geoserver.py
+++ libs/data_manipulation/src/data_manipulation/geoserver.py
@@ -1,6 +1,7 @@
 """GeoServer layer creation utilities."""
 
 from geoservercloud import GeoServerCloud  # type: ignore[import-untyped]
+from geoservercloud.models.datastore import PostGisDataStore
 from geoservercloud.models.featuretype import FeatureType
 from geoservercloud.services import RestService
 from pydantic import BaseModel  # type: ignore[import-untyped]
@@ -48,17 +49,28 @@ def create_workspace(
     if pg_schema is None:
         pg_schema = workspace_name
 
-    # Create workspace
-    geoserver.create_workspace(workspace_name)  # type: ignore[reportUnknownMemberType]
+    if geoserver.get_workspace(workspace_name)[1] != 200:
+        # Create workspace
+        geoserver.create_workspace(workspace_name)  # type: ignore[reportUnknownMemberType]
 
+    namespace = geoserver.rest_service.rest_client.get(f"/rest/namespaces/{workspace_name}").json()[
+        "namespace"
+    ]["uri"]
     # Create JNDI datastore
-    geoserver.create_jndi_datastore(  # type: ignore[reportUnknownMemberType]
-        workspace_name=workspace_name,
-        datastore_name=datastore_name,
-        jndi_reference=jndi_reference,
-        pg_schema=pg_schema,
+    datastore = PostGisDataStore(
+        workspace_name,
+        datastore_name,
+        connection_parameters={
+            "dbtype": "postgis",
+            "jndiReferenceName": jndi_reference,
+            "schema": pg_schema,
+            "namespace": namespace,
+            "Expose primary keys": "true",
+        },
+        type="PostGIS (JNDI)",
         description=description,
     )
+    geoserver.rest_service.create_jndi_datastore(workspace_name=workspace_name, datastore=datastore)
 
     return WorkspaceCreationResult(
         workspace=workspace_name,
